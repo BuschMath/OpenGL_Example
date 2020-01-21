aceeca636c273cd5e4b4d477009b53af402d082f
David Busch <busch@users.noreply.github.com>
2020-01-21 10:30:25 -0600

    Complete Hello World Tutorial

--- OpenGL_Example/src/main.cpp
+++ OpenGL_Example/src/main.cpp
@@ -7,6 +7,12 @@
 const int windowWidth = 800;
 const int windowHeight = 600;
 
+// Helper function prototypes ********************************
+// Deals with window resizing, updating the rendering window
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+void processInput(GLFWwindow* window);
+
 int main()
 {
 	// Initialize GLFW
@@ -29,5 +35,51 @@ int main()
 	// Make window the current context
 	glfwMakeContextCurrent(window);
 
+	// Initialize GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	// Set the size of the rendering window
+	// This maps the openGL render space from -1 to 1 over to 0 to windowWidth and same for height
+	glViewport(0, 0, windowWidth, windowHeight);
+
+	// Register window resize helper function with glfw
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// Render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// Input
+		processInput(window);
+
+		// Rendering
+		// Set clear color state
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		// Color the buffer to match clear color state
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Load image buffer to display
+		glfwSwapBuffers(window);
+		// Check to see if any events have occured and deal with them
+		glfwPollEvents();
+	}
+
+	// Clean-up GLFW
+	glfwTerminate();
+
 	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
 }
