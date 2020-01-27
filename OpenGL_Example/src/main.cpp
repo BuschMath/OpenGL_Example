8737a78b3cbfce414302899ed4513b8589cd18f4
David Busch <busch@users.noreply.github.com>
2020-01-27 12:24:26 -0600

    Every third cube, rotate.

--- OpenGL_Example/src/main.cpp
+++ OpenGL_Example/src/main.cpp
@@ -99,22 +99,14 @@ int main()
 		-0.5f,  0.5f,  0.5f,	1.0f, 0.0f,		// 8
 		-0.5f,  0.5f, -0.5f,	1.0f, 1.0f,		// 9
 		-0.5f, -0.5f, -0.5f,	0.0f, 1.0f,		// 10
-		-0.5f, -0.5f,  0.5f,	0.0f, 0.0f,		// 11
 		// Right face
-		 0.5f,  0.5f,  0.5f,	1.0f, 0.0f,		// 12
-		 0.5f,  0.5f, -0.5f,	1.0f, 1.0f,		// 13
-		 0.5f, -0.5f, -0.5f,	0.0f, 1.0f,		// 14
-		 0.5f, -0.5f,  0.5f,	0.0f, 0.0f,		// 15
+		 0.5f,  0.5f,  0.5f,	1.0f, 0.0f,		// 11
+		 0.5f, -0.5f, -0.5f,	0.0f, 1.0f,		// 12
+		 0.5f, -0.5f,  0.5f,	0.0f, 0.0f,		// 13
 		 // Bottom face
-		-0.5f, -0.5f, -0.5f,	0.0f, 1.0f,		// 16
-		 0.5f, -0.5f, -0.5f,	1.0f, 1.0f,		// 17
-		 0.5f, -0.5f,  0.5f,	1.0f, 0.0f,		// 18
-		-0.5f, -0.5f,  0.5f,	0.0f, 0.0f,		// 19
+		 0.5f, -0.5f, -0.5f,	1.0f, 1.0f,		// 14
 		// Top face
-		-0.5f,  0.5f, -0.5f,	0.0f, 1.0f,		// 20
-		 0.5f,  0.5f, -0.5f,	1.0f, 1.0f,		// 21
-		 0.5f,  0.5f,  0.5f,	1.0f, 0.0f,		// 22
-		-0.5f,  0.5f,  0.5f,	0.0f, 0.0f 		// 23
+		-0.5f,  0.5f,  0.5f,	0.0f, 0.0f 		// 15
 	};
 
 	// ************************************************
@@ -129,16 +121,16 @@ int main()
 		6, 7, 4,			// Second triangle
 		// Left face
 		8, 9, 10,			// First triangle
-		10, 11, 8,			// Second triangle
+		10, 4, 8,			// Second triangle
 		// Right face
-		12, 13, 14,			// First triangle
-		14, 15, 12,			// Second triangle
+		11, 2, 12,			// First triangle
+		12, 13, 11,			// Second triangle
 		// Bottom face
-		16, 17, 18,			// First triangle
-		18, 19, 16,			// Second triangle
+		10, 14, 5,			// First triangle
+		5, 4, 10,			// Second triangle
 		// Top face
-		20, 21, 22,			// First triangle
-		22, 23, 20			// Second triangle
+		3, 2, 11,			// First triangle
+		11, 15, 3			// Second triangle
 	};
 
 	// ************************************************
@@ -206,6 +198,19 @@ int main()
 
 	glEnable(GL_DEPTH_TEST);
 
+	glm::vec3 cubePositions[] = {
+		glm::vec3(0.0f,  0.0f,  0.0f),
+		glm::vec3(2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f,  2.0f, -2.5f),
+		glm::vec3(1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
+
 	// ************************************************
 	// Render loop
 	// ************************************************
@@ -225,24 +230,29 @@ int main()
 		// ************************************************
 		// Send MVP matrix uniforms
 		// ************************************************
-		glm::mat4 model = glm::mat4(1.0f);
-		model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
-		int modelLoc = glGetUniformLocation(shader.GetID(), "model");
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		for (unsigned int i = 0; i < 10; i++)
+		{
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			if(i % 3 == 0)
+				model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+			int modelLoc = glGetUniformLocation(shader.GetID(), "model");
+			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
-		int viewLoc = glGetUniformLocation(shader.GetID(), "view");
-		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+			int viewLoc = glGetUniformLocation(shader.GetID(), "view");
+			glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
 
-		int projectionLoc = glGetUniformLocation(shader.GetID(), "projection");
-		glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+			int projectionLoc = glGetUniformLocation(shader.GetID(), "projection");
+			glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, texture0.GetID());
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, texture1.GetID());
-		VAO.Bind();
-		glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
-		VAO.Unbind();
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, texture0.GetID());
+			glActiveTexture(GL_TEXTURE1);
+			glBindTexture(GL_TEXTURE_2D, texture1.GetID());
+			VAO.Bind();
+			glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+			VAO.Unbind();
+		}
 
 		// Load image buffer to display
 		glfwSwapBuffers(window);
