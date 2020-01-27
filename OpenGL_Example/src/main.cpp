c8e8bfef44096bfc22f0486c282098c770b45db9
BuschMath <47864618+BuschMath@users.noreply.github.com>
2020-01-26 18:06:40 -0600

    Efficient cube.

--- OpenGL_Example/src/main.cpp
+++ OpenGL_Example/src/main.cpp
@@ -18,7 +18,7 @@
 // Global constants ********************************
 const int windowWidth = 800;
 const int windowHeight = 600;
-const int numberOfVertices = 36;
+const int numberOfVertices = 24;
 const int numberOfDataPerVertex = 5;
 const int numberOfData = numberOfVertices * numberOfDataPerVertex;
 
@@ -84,49 +84,37 @@ int main()
 	// ************************************************
 	// Set vertices of a triangle in normalized device coordinates
 	float vertices[] = {
-		// Vertices				// Texture
+		// Vertices				// Texture		// Ver
 		// Back face
-		-0.5f, -0.5f, -0.5f,	0.0f, 0.0f,
-		 0.5f, -0.5f, -0.5f,	1.0f, 0.0f,
-		 0.5f,  0.5f, -0.5f,	1.0f, 1.0f,
-		 0.5f,  0.5f, -0.5f,	1.0f, 1.0f,
-		-0.5f,  0.5f, -0.5f,	0.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,	0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,	0.0f, 0.0f,		// 0
+		 0.5f, -0.5f, -0.5f,	1.0f, 0.0f,		// 1
+		 0.5f,  0.5f, -0.5f,	1.0f, 1.0f,		// 2
+		-0.5f,  0.5f, -0.5f,	0.0f, 1.0f,		// 3
 		// Front face
-		-0.5f, -0.5f,  0.5f,	0.0f, 0.0f,
-		 0.5f, -0.5f,  0.5f,	1.0f, 0.0f,
-		 0.5f,  0.5f,  0.5f,	1.0f, 1.0f,
-		 0.5f,  0.5f,  0.5f,	1.0f, 1.0f,
-		-0.5f,  0.5f,  0.5f,	0.0f, 1.0f,
-		-0.5f, -0.5f,  0.5f,	0.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,	0.0f, 0.0f,		// 4
+		 0.5f, -0.5f,  0.5f,	1.0f, 0.0f,		// 5
+		 0.5f,  0.5f,  0.5f,	1.0f, 1.0f,		// 6
+		-0.5f,  0.5f,  0.5f,	0.0f, 1.0f,		// 7
 		// Left face
-		-0.5f,  0.5f,  0.5f,	1.0f, 0.0f,
-		-0.5f,  0.5f, -0.5f,	1.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,	0.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,	0.0f, 1.0f,
-		-0.5f, -0.5f,  0.5f,	0.0f, 0.0f,
-		-0.5f,  0.5f,  0.5f,	1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,	1.0f, 0.0f,		// 8
+		-0.5f,  0.5f, -0.5f,	1.0f, 1.0f,		// 9
+		-0.5f, -0.5f, -0.5f,	0.0f, 1.0f,		// 10
+		-0.5f, -0.5f,  0.5f,	0.0f, 0.0f,		// 11
 		// Right face
-		 0.5f,  0.5f,  0.5f,	1.0f, 0.0f,
-		 0.5f,  0.5f, -0.5f,	1.0f, 1.0f,
-		 0.5f, -0.5f, -0.5f,	0.0f, 1.0f,
-		 0.5f, -0.5f, -0.5f,	0.0f, 1.0f,
-		 0.5f, -0.5f,  0.5f,	0.0f, 0.0f,
-		 0.5f,  0.5f,  0.5f,	1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,	1.0f, 0.0f,		// 12
+		 0.5f,  0.5f, -0.5f,	1.0f, 1.0f,		// 13
+		 0.5f, -0.5f, -0.5f,	0.0f, 1.0f,		// 14
+		 0.5f, -0.5f,  0.5f,	0.0f, 0.0f,		// 15
 		 // Bottom face
-		-0.5f, -0.5f, -0.5f,	0.0f, 1.0f,
-		 0.5f, -0.5f, -0.5f,	1.0f, 1.0f,
-		 0.5f, -0.5f,  0.5f,	1.0f, 0.0f,
-		 0.5f, -0.5f,  0.5f,	1.0f, 0.0f,
-		-0.5f, -0.5f,  0.5f,	0.0f, 0.0f,
-		-0.5f, -0.5f, -0.5f,	0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,	0.0f, 1.0f,		// 16
+		 0.5f, -0.5f, -0.5f,	1.0f, 1.0f,		// 17
+		 0.5f, -0.5f,  0.5f,	1.0f, 0.0f,		// 18
+		-0.5f, -0.5f,  0.5f,	0.0f, 0.0f,		// 19
 		// Top face
-		-0.5f,  0.5f, -0.5f,	0.0f, 1.0f,
-		 0.5f,  0.5f, -0.5f,	1.0f, 1.0f,
-		 0.5f,  0.5f,  0.5f,	1.0f, 0.0f,
-		 0.5f,  0.5f,  0.5f,	1.0f, 0.0f,
-		-0.5f,  0.5f,  0.5f,	0.0f, 0.0f,
-		-0.5f,  0.5f, -0.5f,	0.0f, 1.0f
+		-0.5f,  0.5f, -0.5f,	0.0f, 1.0f,		// 20
+		 0.5f,  0.5f, -0.5f,	1.0f, 1.0f,		// 21
+		 0.5f,  0.5f,  0.5f,	1.0f, 0.0f,		// 22
+		-0.5f,  0.5f,  0.5f,	0.0f, 0.0f 		// 23
 	};
 
 	// ************************************************
@@ -135,22 +123,22 @@ int main()
 	unsigned int indices[] = {
 		// Back face
 		0, 1, 2,			// First triangle
-		3, 4, 5,			// Second triangle
+		2, 3, 0,			// Second triangle
 		// Front face
-		6, 7, 8,			// First triangle
-		9, 10, 11,			// Second triangle
+		4, 5, 6,			// First triangle
+		6, 7, 4,			// Second triangle
 		// Left face
-		12, 13, 14,			// First triangle
-		15, 16, 17,			// Second triangle
+		8, 9, 10,			// First triangle
+		10, 11, 8,			// Second triangle
 		// Right face
-		18, 19, 20,			// First triangle
-		21, 22, 23,			// Second triangle
+		12, 13, 14,			// First triangle
+		14, 15, 12,			// Second triangle
 		// Bottom face
-		24, 25, 26,			// First triangle
-		27, 28, 29,			// Second triangle
+		16, 17, 18,			// First triangle
+		18, 19, 16,			// Second triangle
 		// Top face
-		30, 31, 32,			// First triangle
-		33, 34, 35			// Second triangle
+		20, 21, 22,			// First triangle
+		22, 23, 20			// Second triangle
 	};
 
 	// ************************************************
